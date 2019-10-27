92a2495df1076675a795057aebd74cdea1f6932c
Guy Newbury <guyn@live.co.uk>
2019-10-27 10:06:12 +0000

    Re-implement initialisation of category API

--- ynab/lib/category_api.py
+++ ynab/lib/category_api.py
@@ -1,22 +1,54 @@
-class category:
+class Category:
+
+    __id = None
+    __category_group_id = None
+    __name = None
+    __hidden = None
+    __originial_category_group_id = None
+    __note = None
+    __budgeted = None
+    __activity = None
+    __balance = None
+    __goal_type = None
+    __goal_creation_month = None
+    __goal_target = None
+    __goal_target_month = None
+    __goal_percentage_complete = None
+    __deleted = None
 
     def __init__(
         self,
-        name='',
-        parent='',
-        catType='',
-        children=[],
-        budgeted=0
+        id,
+        category_group_id,
+        name,
+        hidden,
+        original_category_group_id,
+        note,
+        budgeted,
+        activity,
+        balance,
+        goal_type,
+        goal_creation_month,
+        goal_target,
+        goal_target_month,
+        goal_percentage_complete,
+        deleted
     ):
-        self.name = name
-        self.type = catType
-        if parent and children:
-            print('Must only have parent or child')
-        elif parent:
-            self.parent = parent
-            self.budgeted = budgeted
-        elif children:
-            self.children = children
+        self.__id = id
+        self.__category_group_id = category_group_id
+        self.__name = name
+        self.__hidden = hidden
+        self.__originial_category_group_id = original_category_group_id
+        self.__note = note
+        self.__budgeted = budgeted
+        self.__activity = activity
+        self.__balance = balance
+        self.__goal_type = goal_type
+        self.__goal_creation_month = goal_creation_month
+        self.__goal_target = goal_target
+        self.__goal_target_month = goal_target_month
+        self.__goal_percentage_complete = goal_percentage_complete
+
 
     def addChild(self, child):
         self.children.append(child)
